30219e604fb18d0cd129462a8fb6fc31ee1b9151
Hatem Ali <hatemali716@gmail.com>
2018-11-19 13:24:08 +0200

    Update Main.java

--- Factory database design pattern/src/Main.java
+++ Factory database design pattern/src/Main.java
@@ -11,29 +11,29 @@ public class Main {
 		Connection conn=new DBFactoryConnection(databasename).getConnection();
 		String selectQuery="select * from books";
 		ResultSet First_Database_table=executeDbQuery(selectQuery,conn);
-        System.out.println("first table content : ");
-        System.out.println("-------------------------------------------");
+       	        System.out.println("first table content : ");
+                System.out.println("-------------------------------------------");
 		while(First_Database_table.next()) {   // Move the cursor to the next row, return false if no more row
-    	    String title = First_Database_table.getString("title");
-            String    author   = First_Database_table.getString("author");
-            System.out.println("book title : "+title + " - AUTHOR : " + author);		    
-        }
+		    String title = First_Database_table.getString("title");
+		    String    author   = First_Database_table.getString("author");
+		    System.out.println("book title : "+title + " - AUTHOR : " + author);		    
+       	        }
 		String databasename2="second";
 		Connection conn2=new DBFactoryConnection(databasename2).getConnection();
 		String selectQuery2="select * from books";
 		ResultSet Second_Database_table=executeDbQuery(selectQuery2,conn2);
 		System.out.println("second table content : ");
-	    System.out.println("-------------------------------------------");
+	        System.out.println("-------------------------------------------");
 		while(Second_Database_table.next()) {   // Move the cursor to the next row, return false if no more row
-    	    String title = Second_Database_table.getString("title");
-    	    String    author   = Second_Database_table.getString("author");
-            System.out.println("book title : "+title + " - AUTHOR : " + author);		    
-        }
+		    String title = Second_Database_table.getString("title");
+		    String    author   = Second_Database_table.getString("author");
+		    System.out.println("book title : "+title + " - AUTHOR : " + author);		    
+                }
 	}
 	
 	public static ResultSet executeDbQuery(String query,Connection conn) throws SQLException{
-        Statement statement = conn.createStatement();
-        ResultSet res = statement.executeQuery(query);
-        return res;
-    }
+		Statement statement = conn.createStatement();
+		ResultSet res = statement.executeQuery(query);
+		return res;
+        }
 }
